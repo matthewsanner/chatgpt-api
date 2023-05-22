8129b4f5d588a5597a10d6a58b34ae37fd615fc3
Matthew Sanner <gravityskunk@gmail.com>
2023-05-22 14:19:48 -0600

    added chat model

--- app.py
+++ app.py
@@ -4,10 +4,21 @@ config = dotenv_values(".env")
 
 openai.api_key = config["API_KEY"]
 
-response = openai.Completion.create(
-    model="text-davinci-003",
-    prompt="5 most populated cities in the world, in order: ",
-    max_tokens=100
+# Completion format
+# response = openai.Completion.create(
+#     model="text-davinci-003",
+#     prompt="5 most populated cities in the world, in order: ",
+#     max_tokens=100
+# )
+
+# Chat format
+response = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    messages=[
+        {"role": "user", "content": "generate a trivia question and answer"}
+    ],
+    temperature=1,  # Default is 1
+    max_tokens=200  # Default is infinite in chat format
 )
 
-print(response.choices[0].text)
+print(response.choices[0].message.content)
